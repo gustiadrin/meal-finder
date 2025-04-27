f183bc5947b427fcdf89e134ae391056da5c02be
gustavo <gustavoadrianvillalba@gmail.com>
2025-04-28 00:15:11 +0200

    Primer commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
-// import { useState } from "react";
-// import SideNav from "./components/SideNav";
+import { useState } from "react";
+import SideNav from "./components/SideNav";
 // import PrincipalContent from "./components/PrincipalContent";
 import Header from "./components/Header";
 
 export default function Home() {
-  // const [category, setCategory] = useState("Beef");
+  const [category, setCategory] = useState("Beef");
   return (
     <>
       <Header></Header>
       <main className="flex flex-auto">
-        {/* <SideNav setCategory={setCategory}></SideNav>
-        <PrincipalContent meal={category}></PrincipalContent> */}
+        <SideNav setCategory={setCategory}></SideNav>
+        {/* <PrincipalContent meal={category}></PrincipalContent> */}
         <p>hola</p>
       </main>
     </>
